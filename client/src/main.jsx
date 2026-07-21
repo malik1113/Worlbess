c58fa9f38789aecdbb5f58a8a3e2ad3db4c27540
malik1113 <malikroberts007@gmail.com>
2026-07-21 14:32:14 -0400

    Add Helmet provider for SEO metadata

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,19 +1,22 @@
-import { StrictMode } from "react"
-import { createRoot } from "react-dom/client"
-import "./index.css"
-import App from "./App.jsx"
-import { CartProvider } from "./context/CartContext.jsx"
-import { AuthProvider } from "./context/AuthContext.jsx"
-import { ToastProvider } from "./context/ToastContext.jsx"
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { CartProvider } from "./context/CartContext.jsx";
+import { AuthProvider } from "./context/AuthContext.jsx";
+import { ToastProvider } from "./context/ToastContext.jsx";
+import { HelmetProvider } from "react-helmet-async";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AuthProvider>
-      <ToastProvider>
-        <CartProvider>
-          <App />
-        </CartProvider>
-      </ToastProvider>
-    </AuthProvider>
+    <HelmetProvider>
+      <AuthProvider>
+        <ToastProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </ToastProvider>
+      </AuthProvider>
+    </HelmetProvider>
   </StrictMode>
-)
+);
