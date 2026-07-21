9aa4fd4e01a07615340fd1850ba9433b0d987be2
malik1113 <malikroberts007@gmail.com>
2026-07-21 14:44:49 -0400

    Add SEO metadata to home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { Helmet } from "react-helmet-async"
 import Hero from "../components/Hero"
 import FeaturedCollections from "../components/FeaturedCollections"
 import VideoSection from "../components/VideoSection"
@@ -7,6 +8,15 @@ import FeaturedProducts from "../components/FeaturedProducts"
 function Home() {
   return (
     <>
+      <Helmet>
+        <title>Worlbess | Premium Grabba & Tobacco Leaf</title>
+
+        <meta
+          name="description"
+          content="Discover premium Grabba, natural tobacco leaf, and smoking accessories from Worlbess. Crafted for quality, freshness, and an exceptional smoking experience."
+        />
+      </Helmet>
+
       <Hero />
       <FeaturedCollections />
       <VideoSection />
