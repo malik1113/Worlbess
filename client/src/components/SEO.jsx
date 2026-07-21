c7b0e73a0319ac296ae37f8e09b9936585ab2730
malik1113 <malikroberts007@gmail.com>
2026-07-21 16:05:19 -0400

    Add Twitter card metadata to SEO component

--- client/src/components/SEO.jsx
+++ client/src/components/SEO.jsx
@@ -1,4 +1,4 @@
-import { Helmet } from "react-helmet-async"
+import { Helmet } from "react-helmet-async";
 
 function SEO({ title, description }) {
   return (
@@ -7,24 +7,28 @@ function SEO({ title, description }) {
 
       {description && (
         <>
-          <meta
-            name="description"
-            content={description}
-          />
+          <meta name="description" content={description} />
 
-          <meta
-            property="og:title"
-            content={title}
-          />
+          <meta property="og:title" content={title} />
 
-          <meta
-            property="og:description"
-            content={description}
-          />
+          <meta property="og:description" content={description} />
+          <meta name="twitter:card" content="summary_large_image" />
+
+          <meta name="twitter:title" content={title} />
+
+          <meta name="twitter:description" content={description} />
         </>
       )}
     </Helmet>
-  )
+  );
 }
 
-export default SEO
+export default SEO;
+
+//Why we're using summary_large_image
+//Twitter/X supports several card types.
+//summary_large_image is the recommended format for ecommerce because it displays:
+//Large product or brand image
+//Page title
+//Description
+//Once we add your default social image later in this milestone, shared Worlbess links will have a polished preview across social platforms.
