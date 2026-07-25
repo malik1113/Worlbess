99f106f1f37921f633f7f9861822444d6797921e
malik1113 <malikroberts007@gmail.com>
2026-07-25 08:12:50 -0400

    Fix featured product image URLs

--- client/src/components/FeaturedProducts.jsx
+++ client/src/components/FeaturedProducts.jsx
@@ -82,7 +82,7 @@ function FeaturedProducts() {
                 <div className="flex aspect-square items-center justify-center overflow-hidden bg-gradient-to-br from-[#1d1d1d] to-black">
                   {product.image ? (
                     <img
-                    src={product.image}
+                    src={`${API_URL}${product.image}`}
                     alt={product.name}
                     loading="lazy"
                     decoding="async"
